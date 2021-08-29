dc48ffd9c4e73e0e03d43611b8ff931955f3fc1d
Souvik-Ghosal <62163357+Souvik-Ghosal@users.noreply.github.com>
2021-08-29 12:47:00 +0530

    Add files via upload

---
+++ formhandler.php
@@ -0,0 +1,27 @@
+<?php
+$name =$_POST['name'];
+$visiter_email =$_POST['email'];
+$subject =$_POST['subject'];
+$message =$_POST['message'];
+
+
+$email_from='souvikghosal1997@gmail.com';
+
+$email_subject='New Form Submission';
+
+$email_body="User Name: $name.\n".
+            "User Email: $visitor_email.\n".
+            "Subject: $subject .\n".
+            "User Message: $message .\n";
+ $to ='souvikghosal13@gmail.com' ;
+
+  $headers = "From : $email_from \r\n";
+
+  $headers = "Reply-To : $visitor_email \r\n";
+
+
+
+  mail($to,$email_subject,$email_body,$headers) ;
+
+header("Location: contact.html");
+?>
